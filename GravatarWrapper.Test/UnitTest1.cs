9364a3e4eadba90357d2ae94f72f8b37b5988989
Tommy Parnell <parnell.tommy@hotmail.com>
2014-01-26 01:54:26 -0500

    initial commit

---
+++ GravatarWrapper.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GravatarWrapper.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var req = GravatarWrapper.Request("test@mctest.com", 400);
+            if (req != null)
+            {
+                req.Save(@"c:\users\test\Desktop\bmp.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
---
+++ GravatarWrapper/GravatarWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using RestSharp;
+using System.Drawing;
+using System.IO;
+
+namespace GravatarWrapper
+{
+    public static class GravatarWrapper
+    {
+        private static string HashEmailForGravatar(string email)
+        {
+            email = email.ToLower();
+            //MD
+            var md5Hasher = MD5.Create();
+
+           var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(email));
+ 
+            var sBuilder = new StringBuilder();
+
+            foreach (var x in data)
+            {
+                sBuilder.Append(x.ToString("x2"));
+            }
+
+            return sBuilder.ToString();  // Return the hexadecimal string. 
+        }
+
+        public static Bitmap Request(string email, int size)
+        {
+            var client = new RestClient("http://www.gravatar.com/avatar/" + HashEmailForGravatar(email));
+            var req = new RestRequest(Method.GET);
+            req.AddParameter("d", "%22%22" );
+            req.AddParameter("s", size);
+            var res = client.Execute(req);
+            if (res != null)
+            {
+                var imagebytes = res.RawBytes;
+                var ms = new MemoryStream(imagebytes);
+                return new Bitmap(ms);
+                //img.Save(@"c:\bmp.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            return null;
+        }
+    }
+}
